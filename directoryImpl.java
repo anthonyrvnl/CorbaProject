f0b994c3a52b942dfe46189057288956c819d932
Anthony Ravenel <anthony.rvnl@gmail.com>
2017-01-25 16:15:44 +0100

    gestion serveur

--- directoryImpl.java
+++ directoryImpl.java
@@ -7,6 +7,15 @@ import org.omg.PortableServer.*;
 public class directoryImpl extends directoryPOA {
 	
 	private int number_of_file;
+	private file_list fileList;
+	protected POA _poa;
+	
+	/*
+	 * reference POA
+	 * */
+	public directoryImpl(POA poa){
+		this._poa = poa;
+	}
 	
     public void open_regular_file(regular_fileHolder r, String name, mode m) throws invalid_type_file, no_such_file{
 					
@@ -30,8 +39,8 @@ public class directoryImpl extends directoryPOA {
     }
 
     public int list_files(file_listHolder l){
-		
-		return 0;
+		this.fileList = l.value;
+		return this.number_of_file;
 	}
 
 
--- serveur.java
+++ serveur.java
@@ -16,38 +16,23 @@ public class serveur {
 			ORB orb = ORB.init(args, null);
 			POA poa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
 			poa.the_POAManager().activate();
-
-			/*
-			usineImpl us = new usineImpl(poa);
-  	             	org.omg.CORBA.Object refus  = poa.servant_to_reference(us);
-
+			
+			directoryImpl dir = new directoryImpl(poa);
+			org.omg.CORBA.Object ref = poa.servant_to_reference(dir);
+			
 			org.omg.CORBA.Object obj = null;
-	               	obj = orb.resolve_initial_references("NameService");
-       			if(obj == null)
-				{
-				System.out.println("Reference nil sur `NameService'");
+			obj = orb.resolve_initial_references("NameService");
+			if(obj == null){
+				System.out.println("Reference null sur NameService");
 				System.exit(1);
-				}
-
-			NamingContext nc = NamingContextHelper.narrow(obj);
-			if(nc == null)
-				{
-				System.out.println("Reference type nil sur `NameService'");
-				System.exit(1);
-				}
-
-			NameComponent[] aName = new NameComponent[1];
-		       	aName[0] = new NameComponent();
-		       	aName[0].id = "formations professionnelles";
-		       	aName[0].kind = "usine";
-
-          		nc.rebind(aName, refus);
+			}
+			
 
 			System.out.println("Le serveur est pret ");
 			orb.run();
       
   
-			System.exit(0);*/
+			System.exit(0);
 		}
 		catch(org.omg.CORBA.ORBPackage.InvalidName ex){
 			System.out.println("org.omg.CORBA.ORBPackage.InvalidName sur orb.resolve_initial_references");
@@ -57,6 +42,10 @@ public class serveur {
 			System.out.println("org.omg.PortableServer.POAManagerPackage.AdapterInactive sur activate() ");
 			System.exit(1);
 		}
+		catch(Exception e){
+			System.out.println(e);
+		}
+		
 
 	}
 
